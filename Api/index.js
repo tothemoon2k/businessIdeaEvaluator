33343e4211e2e1e822b6880390e78d2a708e98ad
Justin Garner <charlottewebsolutions1@gmail.com>
2024-03-03 23:15:08 -0500

    Fixed node_modules

--- Api/index.js
+++ Api/index.js
@@ -1,12 +1,32 @@
+//Setup Express
 const express = require('express');
 const app = express();
-
 app.use(express.json());
 const port = 3000;
 
-app.post('/evaluate', (req, res) => {
-    res.send(req.body)
-    console.log();
+//Get Env Variables
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
+
+//Configure OpenAI
+const OpenAI = require('openai');
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY, // This is the default and can be omitted
+});
+
+//Prompt Helper Func
+const { generatePrompt } = require('./promptHelper');
+
+
+app.post('/evaluate', async (req, res) => {
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: generatePrompt(req.body.businessIdea) }],
+        model: 'gpt-4',
+    });
+
+    console.log(chatCompletion.choices[0].message.content);
+    
+    res.send("Completed")
   })
 
 app.get('/', (req, res) => {
