566efca54a28b9ef90431efa6776e6df1616455d
Justin Garner <charlottewebsolutions1@gmail.com>
2024-03-14 16:43:22 -0400

    Added CORS

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 //Setup Express
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 const port = 3000;
 
 //Get Env Variables
